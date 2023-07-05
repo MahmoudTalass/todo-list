ffc95de50c18b28be654a89782f426941e23dd31
Mahmoud <mahmoud.talass@gmail.com>
2023-07-05 10:32:19 -0400

    Create module to handle individual todo items

---
+++ src/todos.js
@@ -0,0 +1,10 @@
+const todoItem = (title, description, duedate, priority, isComplete, id) => {
+    return {
+        title,
+        description,
+        duedate,
+        priority,
+        isComplete,
+        id
+    }
+}
