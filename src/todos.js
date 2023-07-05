40f7ddfe62defeb251fbdee1df1abe8b9ab92851
Mahmoud <mahmoud.talass@gmail.com>
2023-07-05 10:52:22 -0400

    Make todo function factory and creation logic

--- src/todos.js
+++ src/todos.js
@@ -1,4 +1,8 @@
-const todoItem = (title, description, duedate, priority, isComplete, id) => {
+let todoId = 0;
+
+const TodoItem = (title, description, duedate, priority) => {
+    let isComplete = false;
+    const id = todoId++;
     return {
         title,
         description,
@@ -7,4 +11,13 @@ const todoItem = (title, description, duedate, priority, isComplete, id) => {
         isComplete,
         id
     }
+}
+
+function createTodoItem() {
+    const title = prompt("Whats the title of your todo?")
+    const description = prompt('what is its description?')
+    const duedate = prompt("when is it due?")
+    const priority = prompt("what is its priority? urgent/normal ")
+
+    return TodoItem(title, description, duedate, priority)
 }
