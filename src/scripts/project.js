234a307532e6e2f44901edc22222beb48a1bfb25
Mahmoud <mahmoud.talass@gmail.com>
2023-07-06 15:26:30 -0400

    Create function to change title of todo

--- src/scripts/project.js
+++ src/scripts/project.js
@@ -14,15 +14,14 @@ const Project = (title) => {
    return {
       todosList: state.todos,
       title: state.title,
-      projectId: state.id,
-      todoIds: state.todosIds
+      id: state.id,
+      todoIds: state.todosIds,
    };
 };
 
 function createProject(title) {
    return Project(title);
 }
-
 
 function addTodoToProject(projectId) {
    let selectedProject = users[0].projects[projectId];
@@ -33,8 +32,14 @@ function addTodoToProject(projectId) {
 
 function removeTodo(projectId, todoId) {
    let projContainingTodo = users[0].projects[projectId].todosList;
-   let indexOfTodo = projContainingTodo.find(todo => todo.id === todoId)
-   users[0].projects[projectId].todosList.splice(indexOfTodo, 1);
+   let todoIndex = projContainingTodo.findIndex((todo) => todo.id === todoId);
+   projContainingTodo.splice(todoIndex, 1);
+}
+
+function editTodoTitle(projectId, todoId, newTitle) {
+   let projContainingTodo = users[0].projects[projectId].todosList;
+   let todoIndex = projContainingTodo.findIndex((todo) => todo.id === todoId);
+   projContainingTodo.todosList[todoIndex].title = newTitle;
 }
 
 export { createProject, addTodoToProject, removeTodo };
