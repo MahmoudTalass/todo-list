104db315fd8e2cfd2b0b2cc3013603a6d702abeb
Mahmoud <mahmoud.talass@gmail.com>
2023-07-06 15:29:52 -0400

    Add all object parameters to its state

--- src/scripts/todo.js
+++ src/scripts/todo.js
@@ -2,13 +2,17 @@ const TodoItem = (title, description, duedate, priority, todoIds) => {
    let state = {
       isComplete: false,
       id: todoIds,
+      title: title,
+      description: description,
+      duedate: duedate,
+      priority: priority
    };
 
    return {
-      title,
-      description,
-      duedate,
-      priority,
+      title: state.title,
+      description: state.description,
+      duedate: state.duedate,
+      priority: state.priority,
       isComplete: state.isComplete,
       id: state.id
    };
