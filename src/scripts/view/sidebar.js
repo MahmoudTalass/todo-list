258d3b38c3c2cc9fd0010ca3cde38502432df439
Mahmoud <mahmoud.talass@gmail.com>
2023-07-07 22:07:44 -0400

    Create module to load sidebar of page
    Sidebar is responsible for tabbing through projects and add projects btn

--- src/scripts/view/sidebar.js
+++ src/scripts/view/sidebar.js
@@ -1,3 +1,44 @@
-const sidebar = document.createElement("div")
-sidebar.classList.add('sidebar')
+import plusIcon from "../../assets/icons/plus.svg"
 
+const body = document.querySelector("body");
+
+function renderSidebar() {
+   const sidebar = document.createElement("div");
+   sidebar.classList.add("sidebar");
+
+   const sidebarElCont = document.createElement("div");
+   sidebarElCont.classList.add("sidebar-element-container");
+
+   const allTodos = document.createElement("div");
+   sidebarElCont.classList.add("all-todos");
+   allTodos.textContent = "All";
+
+   const projectsContHeading = document.createElement("div");
+   projectsContHeading.classList.add("projects-container-heading");
+
+   const projectsHeading = document.createElement("p");
+   projectsHeading.classList.add("projects-heading");
+   projectsHeading.textContent = "Projects";
+
+   const addProjectBtn = document.createElement("button");
+   addProjectBtn.classList.add("add-project-btn");
+   addProjectBtn.textContent = plusIcon;
+   addProjectBtn.id = "add-project-btn";
+
+   const projectsContainer = document.createElement("div");
+   projectsContainer.classList.add("projects-container");
+   projectsContainer.id = "projects-container";
+
+   projectsContHeading.appendChild(projectsHeading);
+   projectsContHeading.appendChild(addProjectBtn);
+
+   sidebarElCont.appendChild(allTodos);
+   sidebarElCont.appendChild(projectsContHeading);
+   sidebarElCont.appendChild(projectsContainer);
+
+   sidebar.appendChild(sidebarElCont);
+
+   body.appendChild(sidebar)
+}
+
+export default renderSidebar
