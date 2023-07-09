7e3bca4ae266a59c6ffe62dd6ce73719c82acf71
Mahmoud <mahmoud.talass@gmail.com>
2023-07-09 11:59:38 -0400

    Create projects module responsible for projects

---
+++ src/scripts/view/projects.js
@@ -0,0 +1,11 @@
+const body = document.querySelector("body");
+
+function renderProjectsContainer() {
+   const main = document.createElement("main");
+   main.id = "projects-container";
+   main.classList.add("projects-container");
+
+   body.appendChild(main);
+}
+
+export { renderProjectsContainer };
