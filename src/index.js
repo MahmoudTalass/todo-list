0d6b8c7e675962f4295f1874b97c9fd1df8ad522
Mahmoud <mahmoud.talass@gmail.com>
2023-07-11 15:47:30 -0400

    Import and load intialLoad and handleProject functions
    Initial load to create the user and handle project to make changes
    to the user and the page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import renderPage from "./scripts/view/load-site";
+
 import reset from "./assets/css/reset.css"
 import generalPageStyle from "./assets/css/general-page.css";
 import headerStyle from "./assets/css/header.css"
@@ -6,6 +7,10 @@ import sidebarStyle from "./assets/css/sidebar.css"
 import projectStyle from "./assets/css/project.css";
 import todoStyle from "./assets/css/todo.css"
 
+import initialLoad from "./scripts/controller/init-load";
+import handleProjects from "./scripts/controller/handle-projects";
 
 
+initialLoad()
 renderPage()
+handleProjects()
