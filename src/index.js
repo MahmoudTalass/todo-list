7b49ea709011ed8590cdb242ed5c95a524f2f38e
Mahmoud <mahmoud.talass@gmail.com>
2023-07-10 21:08:46 -0400

    Import all css style sheets

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import renderPage from "./scripts/view/load-site";
+import reset from "./assets/css/reset.css"
+import headerStyle from "./assets/css/header.css"
+import sidebarStyle from "./assets/css/sidebar.css"
+import generalPageStyle from "./assets/css/general-page.css"
+import todoStyle from "./assets/css/todo.css"
+import projectStyle from "./assets/css/project.css"
+
+
+
+renderPage()
