b1d8453cd24fe50004f2b8713ed7dd7ca20f863d
Mahmoud <mahmoud.talass@gmail.com>
2023-07-11 15:28:14 -0400

    Create event listner on add project button to displays form

--- src/scripts/controller/handle-projects.js
+++ src/scripts/controller/handle-projects.js
@@ -1,12 +1,21 @@
-const addProjectBtn = document.querySelector("#add-project-btn");
+function handleProjects() {
+   const addProjectBtn = document.querySelector("#add-project-btn");
 
-const addProjectForm = document.querySelector("#add-project-form");
-const addProjectFormModal = document.querySelector("#add-project-form-modal");
+   const addProjectForm = document.querySelector("#add-project-form");
+   const addProjectFormModal = document.querySelector(
+      "#add-project-form-modal"
+   );
 
-addProjectBtn.addEventListener(() => {
-    addProjectFormModal.display = "block"
-})
+   addProjectBtn.addEventListener("click", () => {
+      console.log("yes");
+      addProjectFormModal.style.display = "block";
+   });
 
+   window.addEventListener("click", (e) => {
+      if (e.target === addProjectFormModal) {
+         addProjectFormModal.style.display = "none";
+      }
+   });
+}
 
-
-
+export default handleProjects;
