23ccb1606c0a326896c95b434c569ecd7abf6a7e
Mahmoud <mahmoud.talass@gmail.com>
2023-07-11 15:16:20 -0400

    Refactor imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import renderPage from "./scripts/view/load-site";
 import reset from "./assets/css/reset.css"
+import generalPageStyle from "./assets/css/general-page.css";
 import headerStyle from "./assets/css/header.css"
 import sidebarStyle from "./assets/css/sidebar.css"
-import generalPageStyle from "./assets/css/general-page.css"
+import projectStyle from "./assets/css/project.css";
 import todoStyle from "./assets/css/todo.css"
-import projectStyle from "./assets/css/project.css"
 
 
 
