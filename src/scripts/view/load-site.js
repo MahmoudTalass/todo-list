1e63c53c7ac3ccc21995e5132de7e0870049c0c8
Mahmoud <mahmoud.talass@gmail.com>
2023-07-11 15:14:25 -0400

    Add dummy content

--- src/scripts/view/load-site.js
+++ src/scripts/view/load-site.js
@@ -1,12 +1,30 @@
 import renderHeader from "./header";
 import renderSidebar from "./sidebar";
-import { renderProjectsContainer } from "./projects";
+import { renderAddProjectForm, renderProject, renderProjectsContainer, renderProjectTitle } from "./projects";
+import { renderTodo, appendTodoToProject, renderAddTodoForm } from "./todos";
 
 function renderPage() {
     renderHeader();
     renderSidebar()
     renderProjectsContainer()
 
+    renderProject(0)
+    renderProjectTitle("myProject", 0)
+    appendTodoToProject(0, renderTodo("task1", "desc", "1/1/2002", "normal", 1))
+
+        renderProjectTitle("myProject", 0);
+
+    appendTodoToProject(
+       0,
+       renderTodo("task1", "desc", "1/1/2002", "normal", 1)
+    );
+     appendTodoToProject(
+        0,
+        renderTodo("task1", "desc", "1/1/2002", "normal", 1)
+     );
+    
+
+    renderAddTodoForm();
 }
 
 export default renderPage;
