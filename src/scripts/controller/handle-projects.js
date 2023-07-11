33fd07cc9daab9394a1c86da38d88cd027042865
Mahmoud <mahmoud.talass@gmail.com>
2023-07-11 15:15:30 -0400

    Create files to control projects and todo behaviors

---
+++ src/scripts/controller/handle-projects.js
@@ -0,0 +1,12 @@
+const addProjectBtn = document.querySelector("#add-project-btn");
+
+const addProjectForm = document.querySelector("#add-project-form");
+const addProjectFormModal = document.querySelector("#add-project-form-modal");
+
+addProjectBtn.addEventListener(() => {
+    addProjectFormModal.display = "block"
+})
+
+
+
+
