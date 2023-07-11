14f4ade762ca4c1476ffbed7e17690bb71c115b6
Mahmoud <mahmoud.talass@gmail.com>
2023-07-11 18:58:21 -0400

    When a project is added, show its screen with the todos
    (if any todos exist yet)

--- src/scripts/controller/handle-projects.js
+++ src/scripts/controller/handle-projects.js
@@ -8,11 +8,14 @@ import { addProject, user } from "../model/user";
 
 function handleProjects() {
    const addProjectBtn = document.querySelector("#add-project-btn");
-
    const addProjectForm = document.querySelector("#add-project-form");
    const addProjectFormModal = document.querySelector(
       "#add-project-form-modal"
    );
+   const projectTitleContainers =
+      document.getElementsByClassName("project-container");
+
+   const projectsContainer = document.querySelector("#projects-container");
 
    // Display add project form
    addProjectBtn.addEventListener("click", () => {
@@ -26,20 +29,33 @@ function handleProjects() {
       }
    });
 
-   /* 
-   * When add project form is submitted, create a card
-   * title slot for the project along with the utility buttons
-   */
+   /*
+    * When add project form is submitted, create a card
+    * title slot for the project along with the utility buttons
+    */
    addProjectForm.addEventListener("submit", (e) => {
       e.preventDefault();
       const projectTitle = getProjectTitleInput();
       addProject(projectTitle);
 
-      let lastAddedProject = user.projects[user.projects.length - 1];
-      renderProjectTitle(projectTitle, lastAddedProject);
+      let lastAddedProjectId = user.projects[user.projects.length - 1].id;
+      renderProjectTitle(projectTitle, lastAddedProjectId);
       clearProjectTitleInput();
       addProjectFormModal.style.display = "none";
+      addEvntListnrsToProj(lastAddedProjectId);
    });
+
+   function addEvntListnrsToProj(lastAddedProjectId) {
+      let lastAddedTitle = projectTitleContainers[lastAddedProjectId];
+      lastAddedTitle.addEventListener("click", () => {
+         let projectId = lastAddedTitle.getAttribute("data-project-title-id");
+         if (projectsContainer.children.length > 0) {
+            projectsContainer.remove(projectsContainer.firstChild);
+         }
+         renderProject(projectId);
+         console.log(projectId);
+      });
+   }
 }
 
 export default handleProjects;
