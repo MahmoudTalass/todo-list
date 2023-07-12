f96d06ff6e2549ab519b3248c73bc030667fa807
Mahmoud <mahmoud.talass@gmail.com>
2023-07-12 18:09:30 -0400

    Before calling init-load render dom elements
    init-load depends on dom elements.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import renderPage from "./scripts/view/load-site";
 
-import reset from "./assets/css/reset.css"
+import reset from "./assets/css/reset.css";
 import generalPageStyle from "./assets/css/general-page.css";
-import headerStyle from "./assets/css/header.css"
-import sidebarStyle from "./assets/css/sidebar.css"
+import headerStyle from "./assets/css/header.css";
+import sidebarStyle from "./assets/css/sidebar.css";
 import projectStyle from "./assets/css/project.css";
-import todoStyle from "./assets/css/todo.css"
+import todoStyle from "./assets/css/todo.css";
 
 import initialLoad from "./scripts/controller/init-load";
-import handleProjects from "./scripts/controller/handle-projects";
+import { handleProjects } from "./scripts/controller/handle-projects";
 
-
-initialLoad()
-renderPage()
-handleProjects()
+renderPage();
+initialLoad();
+handleProjects();
