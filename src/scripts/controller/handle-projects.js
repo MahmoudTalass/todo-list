b320dc9ca31e372fcbd98a164f94f4da2b167bd5
Mahmoud <mahmoud.talass@gmail.com>
2023-07-12 18:08:38 -0400

    Get id of recently added project and addProjectToPage using it
    Refactor code as well

--- src/scripts/controller/handle-projects.js
+++ src/scripts/controller/handle-projects.js
@@ -36,7 +36,8 @@ function handleProjects() {
       e.preventDefault();
       const projectTitle = getProjectTitleInput();
       addProject(projectTitle);
-      addProjectToPage();
+      const recentProjectId = user.projects[user.projects.length - 1].id;
+      addProjectToPage(projectTitle, recentProjectId);
       addProjectFormModal.style.display = "none";
    });
 }
@@ -49,29 +50,28 @@ function addEvntListnrsToProj(recentProjectId) {
 }
 
 function tabIntoProject(e) {
-   const projectTabBtn = document.querySelector("button.project-tab-btn")
+   const projectTabBtn = document.querySelector("button.project-tab-btn");
    if (e.target === projectTabBtn) {
       const projectsContainer = document.querySelector("#projects-container");
       const targetProject = e.target;
       let targetProjectId = targetProject.getAttribute("data-project-title-id");
 
       if (projectsContainer.children.length > 0) {
-          const currentProject = projectsContainer.firstChild;
-          const currentProjectId = currentProject.getAttribute(
-             "data-project-id"
-          );
-          if (currentProjectId === targetProjectId) return;
-         projectsContainer.removeChild();
+         const currentProject = projectsContainer.firstChild;
+         const currentProjectId =
+            currentProject.getAttribute("data-project-id");
+         if (currentProjectId === targetProjectId) return;
+         projectsContainer.removeChild(currentProject);
       }
       renderProject(targetProjectId);
    }
 }
 
-function addProjectToPage(projectTitle) {
-   let recentProjectId = user.projects[user.projects.length - 1].id;
-   renderProjectTitle(projectTitle, recentProjectId);
+function addProjectToPage(projectTitle, projectId) {
+   // let recentProjectId = user.projects[user.projects.length - 1].id;
+   renderProjectTitle(projectTitle, projectId);
    clearProjectTitleInput();
-   addEvntListnrsToProj(recentProjectId);
+   addEvntListnrsToProj(projectId);
 }
 
 export { handleProjects, addProjectToPage };
