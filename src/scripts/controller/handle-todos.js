57539fabd2c7e416b6caa902edc268f160b4556f
Mahmoud <mahmoud.talass@gmail.com>
2023-07-13 10:32:57 -0400

    Create event listeners to show and hide add-todo-form

--- src/scripts/controller/handle-todos.js
+++ src/scripts/controller/handle-todos.js
@@ -1,13 +1,20 @@
 function handleTodosInput() {
-    debugger
-    const project = document.querySelector('[data-project-id')
+   debugger;
+   const project = document.querySelector("[data-project-id");
    const addTodoBtn = document.querySelector("#add-task-btn");
-   const addTodoForm = document.querySelector("#add-todo-form");
    const addTodoFormModal = document.querySelector("#add-todo-form-modal");
+   const addTodoForm = document.querySelector("#add-todo-form");
 
-   addTodoBtn.addEventListener('click', () => {
-        addTodoFormModal.style.display = "block"
+
+   addTodoBtn.addEventListener("click", () => {
+      addTodoFormModal.style.display = "block"
+   });
+
+   window.addEventListener("click", (e) => {
+        if (e.target === addTodoFormModal) {
+            addTodoFormModal.style.display = "none";
+        }
    })
 }
 
-export { handleTodosInput }
+export { handleTodosInput };
