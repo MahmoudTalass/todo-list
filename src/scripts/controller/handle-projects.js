a8d4d2ee283f0bcf7b8e40ac77a9e1a29ce0ac27
Mahmoud <mahmoud.talass@gmail.com>
2023-07-13 12:23:04 -0400

    Include functions that event handlers depend on
    Move event handlers to be in view directory but the functions that
    use model functions stay here to make the changes to the data

--- src/scripts/controller/handle-projects.js
+++ src/scripts/controller/handle-projects.js
@@ -1,59 +1,17 @@
 import {
-   clearProjectTitleInput,
    getProjectTitleInput,
    renderProject,
    renderProjectTitle,
 } from "../view/projects";
 import { addProject, user } from "../model/user";
-import { handleTodosInput } from "./handle-todos";
+import { addEvntListnrsToProj } from "../view/project-events";
+import { handleTodosInput } from "../view/todo-events";
 
-const projectTitleContainers =
-   document.getElementsByClassName("project-container");
-
-function handleProjectsInput() {
-   // Display add project form
-   const addProjectBtn = document.querySelector("#add-project-btn");
-   const addProjectForm = document.querySelector("#add-project-form");
-   const addProjectFormModal = document.querySelector(
-      "#add-project-form-modal"
-   );
-
-   addProjectBtn.addEventListener("click", () => {
-      addProjectFormModal.style.display = "block";
-   });
-
-   // Remove add project form when user clicks on window
-   window.addEventListener("click", (e) => {
-      if (e.target === addProjectFormModal) {
-         addProjectFormModal.style.display = "none";
-      }
-   });
-
-   /*
-    * When add project form is submitted, create a card
-    * title slot for the project along with the utility buttons
-    */
-   addProjectForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const projectTitle = getProjectTitleInput();
-      addProject(projectTitle);
-      const recentProjectId = user.projects[user.projects.length - 1].id;
-      addProjectToPage(projectTitle, recentProjectId);
-      clearProjectTitleInput();
-      addProjectFormModal.style.display = "none";
-   });
-}
-
-/*
- *  Add event lister to last added project title and
- *  give it tabbing ability
- */
-function addEvntListnrsToProj(recentProjectId) {
-   let recentTitle = projectTitleContainers[recentProjectId];
-
-   recentTitle.addEventListener("click", (e) => {
-      tabIntoProject(e);
-   });
+function addProjectUsingForm() {
+   const projectTitle = getProjectTitleInput();
+   addProject(projectTitle);
+   const recentProjectId = user.projects[user.projects.length - 1].id;
+   addProjectToPage(projectTitle, recentProjectId);
 }
 
 /*
@@ -82,9 +40,10 @@ function tabIntoProject(e) {
    }
 }
 
+// Redners the project title to the page and adds event to it
 function addProjectToPage(projectTitle, projectId) {
    renderProjectTitle(projectTitle, projectId);
    addEvntListnrsToProj(projectId);
 }
 
-export { handleProjectsInput, addProjectToPage };
+export { addProjectToPage, tabIntoProject, addProjectUsingForm };
