e572ba10be1f48eddc3787d23bd58de040c68040
Mahmoud <mahmoud.talass@gmail.com>
2023-07-14 14:17:03 -0400

    Create functions to add todo to project and to page

--- src/scripts/controller/handle-todos.js
+++ src/scripts/controller/handle-todos.js
@@ -1,1 +1,50 @@
+import { addTodoToProject, getRecentTodo } from "../model/project";
+import {
+   appendTodoToProject,
+   getTodoDescInput,
+   getTodoDuedateInput,
+   getTodoPriorityInput,
+   getTodoTitleInput,
+   renderTodo,
+} from "../view/todos";
 
+import { user } from "../model/user";
+
+function addTodoUsingForm(projectId) {
+   const todoTitle = getTodoTitleInput();
+   const todoDuedate = getTodoDuedateInput();
+   const todoPriority = getTodoPriorityInput();
+   const todoDesc = getTodoDescInput();
+   debugger;
+   const recentTodoId = getRecentTodo(projectId);
+
+   addTodoToProject(todoTitle, todoDuedate, todoPriority, todoDesc, projectId);
+   addTodoToPage(
+      todoTitle,
+      todoDuedate,
+      todoPriority,
+      todoDesc,
+      recentTodoId,
+      projectId
+   );
+}
+
+function addTodoToPage(
+   todoTitle,
+   todoDuedate,
+   todoPriority,
+   todoDesc,
+   todoId,
+   projectId
+) {
+   const todo = renderTodo(
+      todoTitle,
+      todoDuedate,
+      todoPriority,
+      todoDesc,
+      todoId
+   );
+   appendTodoToProject(todo);
+}
+
+export { addTodoToPage, addTodoUsingForm };
