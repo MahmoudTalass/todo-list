85f071e2adbdf1af0e7bafea645320a27853a63a
Mahmoud <mahmoud.talass@gmail.com>
2023-07-14 17:48:00 -0400

    Adjust import location

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import todoStyle from "./assets/css/todo.css";
 
 import initialLoad from "./scripts/controller/init-load";
 import { handleProjectsInput } from "./scripts/view/project-events";
-import { handleTodosInput } from "./scripts/controller/handle-todos";
+import { handleTodosInput } from "./scripts/controller/todos-controller";
 
 renderPage();
 initialLoad();
