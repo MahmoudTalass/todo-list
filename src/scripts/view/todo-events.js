47acc54f4a68fa75bc0ab3c829d96d4702ae3354
Mahmoud <mahmoud.talass@gmail.com>
2023-07-14 11:01:44 -0400

    Add event listener to add todo form submission
    Also capture the id of the project currently working on

--- src/scripts/view/todo-events.js
+++ src/scripts/view/todo-events.js
@@ -4,7 +4,12 @@ function handleTodosInput() {
    const addTodoFormModal = document.querySelector("#add-todo-form-modal");
    const addTodoForm = document.querySelector("#add-todo-form");
 
+   // Get the project id by checking the data attribute of the
+   // project currently clicked on
+   let projectId = project.getAttribute("data-project-id");
+
    addTodoBtn.addEventListener("click", () => {
+      console.log(projectId);
       addTodoFormModal.style.display = "block";
    });
 
@@ -12,6 +17,11 @@ function handleTodosInput() {
       if (e.target === addTodoFormModal) {
          addTodoFormModal.style.display = "none";
       }
+   });
+
+   addTodoForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      addTodoFormModal.style.display = "none";
    });
 }
 
