68879c641c65333c9c37786c828d3cd716898a07
Mahmoud <mahmoud.talass@gmail.com>
2023-07-17 21:56:22 -0400

    Add event listeners to todo form and edit btn

--- src/scripts/view/todo-events.js
+++ src/scripts/view/todo-events.js
@@ -1,5 +1,5 @@
-import { addTodoUsingForm } from "../controller/todos-controller";
-import { clearAddTodoInputs } from "./todos";
+import { addTodoUsingForm, editTodo } from "../controller/todos-controller";
+import { clearTodoFormInputs } from "./todos";
 
 function handleTodosInput() {
    const projectsContainer = document.querySelector("#projects-container");
@@ -7,44 +7,50 @@ function handleTodosInput() {
    const addTodoFormModal = document.querySelector("#add-todo-form-modal");
    const addTodoForm = document.querySelector("#add-todo-form");
 
-   const editTodoFormModal = document.querySelector("#edit-todo-form-modal")
-   const editTodoForm = document.querySelector("#edit-todo-form")
+   const editTodoFormModal = document.querySelector("#edit-todo-form-modal");
+   const editTodoForm = document.querySelector("#edit-todo-form");
 
    projectsContainer.addEventListener("click", (e) => {
-      const isAddTaskBtn = e.target.classList.contains("add-task-btn")
-      const isEditTaskBtn = e.target.classList.contains("edit-todo-icon")
+      const isAddTaskBtn = e.target.classList.contains("add-task-btn");
+      const isEditTaskBtn = e.target.classList.contains("edit-todo-icon");
 
       if (isAddTaskBtn) {
          addTodoFormModal.style.display = "block";
       }
 
       if (isEditTaskBtn) {
-         editTodoFormModal.style.display = "block"
+         editTodoFormModal.style.display = "block";
+         const todo = e.target.closest(".todo-container");
+         const todoId = todo.getAttribute("data-todo-id");
+         editTodoForm.setAttribute("data-todo-being-edited", todoId);
       }
    });
 
    window.addEventListener("click", (e) => {
       if (e.target === addTodoFormModal) {
          addTodoFormModal.style.display = "none";
-         clearAddTodoInputs("add");
+         clearTodoFormInputs("add");
       }
 
       if (e.target === editTodoFormModal) {
-         editTodoFormModal.style.display = "none"
-         clearAddTodoInputs("edit")
+         editTodoFormModal.style.display = "none";
+         clearTodoFormInputs("edit");
       }
    });
 
    addTodoForm.addEventListener("submit", (e) => {
       e.preventDefault();
       addTodoUsingForm();
-      clearAddTodoInputs();
+      clearTodoFormInputs("add");
       addTodoFormModal.style.display = "none";
    });
 
    editTodoForm.addEventListener("submit", (e) => {
-
-   })
+      e.preventDefault();
+      editTodo();
+      clearTodoFormInputs("edit");
+      editTodoFormModal.style.display = "none";
+   });
 }
 
 export { handleTodosInput };
